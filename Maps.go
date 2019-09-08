58bc020cc90f61458fcdd04aee14e3d9b9964224
Mehmet Emin Kaymaz <50118591+MehmetEminKaymaz@users.noreply.github.com>
2019-09-08 13:07:09 +0300

    Update Maps.go

--- Maps.go
+++ Maps.go
@@ -2,7 +2,7 @@ package EasyReflect
 
 import "reflect"
 
-func newMap(key interface{},value interface{}) (newmp interface{}){
+func NewMap(key interface{},value interface{}) (newmp interface{}){
 
 	newmp=reflect.MakeMap(reflect.MapOf(reflect.TypeOf(key),reflect.TypeOf(value)))
 	return
